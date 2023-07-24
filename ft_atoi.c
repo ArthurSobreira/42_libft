005930734dc62e1ee5e36a5b984f56b027485b45
Arthur Sobreira <tutusobreira@gmail.com>
2023-07-24 19:56:37 -0300

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,13 +6,13 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 19:11:34 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/07/24 19:41:53 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/07/24 19:55:33 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(const char *number)
+int	ft_atoi(const char *nptr)
 {
 	size_t	i;
 	int		result;
@@ -21,20 +21,20 @@ int	ft_atoi(const char *number)
 	i = 0;
 	result = 0;
 	is_negative = 1;
-	while (((number[i] >= 9) && (number[i] <= 13)) || ((number[i]) == 32))
+	while (((nptr[i] >= 9) && (nptr[i] <= 13)) || ((nptr[i]) == 32))
 	{
 		i++;
 	}
-	if ((number[i] == '-') || (number[i] == '+'))
+	if ((nptr[i] == '-') || (nptr[i] == '+'))
 	{
-		if ((number[i] == '-'))
+		if ((nptr[i] == '-'))
 			is_negative *= -1;
 		i++;
 	}
-	while (ft_isdigit(number[i]))
+	while (ft_isdigit(nptr[i]))
 	{
 		result *= 10;
-		result += number[i] - '0';
+		result += nptr[i] - '0';
 		i++;
 	}
 	return (result * is_negative);
