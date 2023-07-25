9128cb1d302c853c4cc638c9f630419d29edd0b2
Arthur Sobreira <arsobrei@c2r11p4.42sp.org.br>
2023-07-25 15:21:29 -0300

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 10:25:27 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/07/25 14:07:04 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/07/25 15:11:52 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,14 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	size_t	full_size;
 
 	full_size = nmemb * size;
+	if (nmemb == 0 || size == 0)
+	{
+		return (malloc(0));
+	}
+	if (full_size > __INT_MAX__ || full_size / nmemb != size)
+	{
+		return (NULL);
+	}
 	allocated_mem = (void *)malloc(full_size);
 	if (allocated_mem == NULL)
 	{
