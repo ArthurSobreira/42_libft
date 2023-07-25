ab573c6f86ca8610c2fb9f194b194bad0e19c7c0
Arthur Sobreira <arsobrei@c1r9p1.42sp.org.br>
2023-07-25 10:20:52 -0300

    Add ft_bzero.c

---
+++ ft_bzero.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/25 09:02:50 by arsobrei          #+#    #+#             */
+/*   Updated: 2023/07/25 10:11:02 by arsobrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t			index;
+	unsigned char	*ptr;
+
+	index = 0;
+	ptr = (unsigned char *)s;
+	while (index < n)
+	{
+		ptr[index] = '\0';
+		index++;
+	}
+	return ;
+}
