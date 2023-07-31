453746cc7e717195a021c417f4b6c92378e3bd00
Arthur <tutusobreira@gmail.com>
2023-07-31 13:33:49 -0300

    Add test_putnbr_fd

--- tests/test_putnbr_fd.c
+++ tests/test_putnbr_fd.c
@@ -1,15 +1,15 @@
 #include "includes/tests.h"
 
-int	test_putchar_fd(char c)
+int	test_putnbr_fd(int c)
 {
 	// Arrange
-    char	buff;
+    char	buff[4096];
     int		file_desc;
     int		byte_nb;
 
     // Act
     file_desc = open("test.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    ft_putchar_fd(c, file_desc);  // Writing on the file
+    ft_putnbr_fd(c, file_desc);  // Writing on the file
 
     // Close the file after writing
     close(file_desc);
@@ -18,19 +18,19 @@ int	test_putchar_fd(char c)
     file_desc = open("test.txt", O_RDONLY);
 
     // Assert
-    byte_nb = read(file_desc, &buff, 1);
+    byte_nb = read(file_desc, buff, sizeof(buff));
     if (byte_nb == -1)
     {
         perror("Error reading from the file");
         return (1);
     }
-    else if (c == buff)
+    else if (c == atoi(buff))
     {
-        printf("\033[32m[Success]\033[0m The character '%c' was written to the file\n", c);
+        printf("\033[32m[Success]\033[0m The number %d was written to the file\n", c);
     }
     else
     {
-        printf("\033[31m[Failure]\033[0m Could not populate the file with the char '%c'\n", c);
+        printf("\033[31m[Failure]\033[0m Could not populate the file with the number %d\n", c);
     }
 
     // Close the file after reading
@@ -41,10 +41,10 @@ int	test_putchar_fd(char c)
 int	main(void)
 {	
 	// Function Call
-	test_putchar_fd('A');
-	test_putchar_fd('Z');
-	test_putchar_fd('1');
-	test_putchar_fd('9');
-	test_putchar_fd('+');
+    test_putnbr_fd(1);
+	test_putnbr_fd(9);
+	test_putnbr_fd(1234);
+	test_putnbr_fd(2147483647);
+	test_putnbr_fd(-2147483648);
 	return (0);
 }
