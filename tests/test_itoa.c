8237cff82d1f64d9e21b8f88ec8f573aafbc560b
Arthur <tutusobreira@gmail.com>
2023-08-01 10:25:01 -0300

    Add test_itoa

--- tests/test_itoa.c
+++ tests/test_itoa.c
@@ -3,41 +3,39 @@
 void	test_itoa(int input)
 {
 	// Arrange
-	const char	*act_result;
-	//const char	*expec_result;
+	char	*act_result;
+	char	expec_result[20];
 
 	// Act
 	act_result = ft_itoa(input);
 
 	// Assert
-	//expec_result = itoa(input);  // Call the original one
+	snprintf(expec_result, sizeof(expec_result), "%d", input);
 	
-	printf("Resultado: %s\n", act_result);
-	//if (strcmp(act_result, expec_result) == 0)
-	//{
-	//	printf("\033[32m[Success]\033[0m Expected: '%s'  Result: '%s'\n", expec_result, act_result);
-	//}
-	//else
-	//{
-	//	printf("\033[31m[Failure]\033[0m Expected: '%s'  Result: '%s'\n", expec_result, act_result);
-	//}
+	if (strcmp(act_result, expec_result) == 0)
+	{
+		printf("\033[32m[Success]\033[0m Expected: '%s'  Result: '%s'\n", expec_result, act_result);
+	}
+	else
+	{
+		printf("\033[31m[Failure]\033[0m Expected: '%s'  Result: '%s'\n", expec_result, act_result);
+	}
 	free((char *)act_result);
-    //free((char *)expec_result);
 }
 
 int	main(void)
 {
-	int	test1 = -22;
-	//int	test2 = ;
-	//int	test3 = ;
-	//int	test4 = ;
-	//int	test5 = ;
+	int	test1 = 0;
+	int	test2 = -9;
+	int	test3 = 1234;
+	int	test4 = 2147483647;
+	int	test5 = -2147483648;
 
 	// Function Call
 	test_itoa(test1);
-	//test_itoa(test2);
-	//test_itoa(test3);
-	//test_itoa(test4);
-	//test_itoa(test5);
+	test_itoa(test2);
+	test_itoa(test3);
+	test_itoa(test4);
+	test_itoa(test5);
 	return(0);
 }
