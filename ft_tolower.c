bea94ef699da0b10b996519972a8d7749aee222f
Arthur Sobreira <tutusobreira@gmail.com>
2023-08-04 15:20:14 -0300

    Update ft_tolower.c

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,9 +6,11 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 15:21:18 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/07/24 19:57:53 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/08/04 15:16:49 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_tolower(int c)
 {
