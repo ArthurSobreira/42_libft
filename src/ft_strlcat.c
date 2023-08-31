6016a80ea6181905ec3419d3ca4c1cc9d3ba0b52
Arthur Sobreira <tutusobreira@gmail.com>
2023-08-31 11:58:26 -0300

    Update ft_strlcat.c

--- src/ft_strlcat.c
+++ src/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 14:01:48 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/07/24 20:02:31 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/08/31 11:56:30 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	dst_index;
 	size_t	src_index;
-	size_t	lenght;
+	size_t	length;
 
-	lenght = ft_strlen(dst) + ft_strlen(src);
+	length = ft_strlen(dst) + ft_strlen(src);
 	dst_index = ft_strlen(dst);
 	src_index = 0;
 	if (size > dst_index)
@@ -30,7 +30,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 			src_index++;
 		}
 		dst[dst_index] = '\0';
-		return (lenght);
+		return (length);
 	}
 	return (ft_strlen(src) + size);
 }
